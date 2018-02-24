f23693c213eb1a8ed9134c7ad99179d88ff15165
mrzivox <35635645+mrzivox@users.noreply.github.com>
2018-02-24 14:01:08 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,105 @@
+// Partie Const ////////////////////////////////////////////////////////////
+
+
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+const prefix = "/"; 
+
+
+
+// Var ////////////////////////////////////////////////////////////
+
+
+  
+  //////////////////////////////////////////////////////////////////  
+  
+    
+  
+  
+  bot.on('message', message => {
+  if (message.content.startsWith(prefix + "test")) {
+  message.channel.send('**Je fonctionne à 100% !**');}});
+  
+  
+  //////////////////////////////////////////////////////////////////  
+  
+  
+  
+  
+  bot.on('message', message => {
+  if (message.content.startsWith(prefix + "bonjour")) {
+    message.channel.send('**Bonjour**' );}});
+  
+  
+  //////////////////////////////////////////////////////////////////  
+  
+  
+  
+  bot.on('message', message => {
+  if (message.content.startsWith(prefix + "aurevoir")) {
+   message.channel.send('**Au revoir**');}});
+  
+  
+  //info///////////////////////////////////////////////////////////
+  
+  bot.on('message', message => {
+    if (message.content.startsWith(prefix + "info")) {
+    var info_embed = new Discord.RichEmbed()
+      .setColor('#00FEC3')
+      .addField("Information Sur le Bot :" , "**Développeur : MrZivox\nVersion : 1.0**")
+      .addField("Actualités :", "**Aucune Actualités**")
+      message.channel.sendEmbed(info_embed)}});
+      var help_embed = new Discord.RichEmbed();
+    
+    
+    
+    
+    
+    ///help///////////////////////////////////////////////////////////  
+    
+    bot.on('message', message => {
+      if (message.content.startsWith(prefix + "staffhelp")) {
+      var help_embed = new Discord.RichEmbed()
+        .setColor('#00FEC3')
+        .addField("**Commandes Principales Staff :**", "**/ban : Permet de bannir un membre .\n/kick : Permet de kick un membre .**")
+        message.channel.sendEmbed(help_embed)}});
+        var help_embed = new Discord.RichEmbed();
+    
+    
+    bot.on('message', message => {
+    if (message.content.startsWith(prefix + "help")) {
+    var help_embed = new Discord.RichEmbed()
+      .setColor('#00FEC3')
+      .addField("**Commandes Principales :**", "**/help : Affiche les commandes du bot .\n/info : Affiche des informations sur le bot .\n**")
+      .addField("**Commandes d'Interaction :**", "**/bonjour : permet de dire bonjour à ZivoxBot .\n/aurevoir : permet de dire au revoir à ZivoxBot .**")
+      message.channel.sendEmbed(help_embed)}});
+      var help_embed = new Discord.RichEmbed();
+    
+    //////////////////////////////////////////////////////////////////
+    
+    bot.on('message', message => {
+    if (message.content.startsWith(prefix + "version")) {
+    message.channel.send('**Version 1.0**');}});
+    
+    
+    
+    
+    ///////////////////////////////////////////////////////////////////
+
+bot.on('message', message => {
+if (message.content.startsWith(prefix + "ping")) {
+var now = require('performance-now');
+var startTime = now();
+message.channel.send("Pong = Attendez...")
+.then(message => {
+var endTime = now();
+return message.edit("**Pong :ping_pong: = " + Math.round(endTime - startTime) + " ms.**");
+}).catch(console.error);
+}});
+
+
+//////////////////////////////////////////////////////////////////
+
+bot.login(process.env.TOKEN);
+
+
